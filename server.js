8659dbab0135eee3e684ea980da454c6cb963f9f
XavierSamuelHuppe <xhuppe@gmail.com>
2017-02-06 21:31:40 -0500

    remove log

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ var app = express()
 
 
 function readJSONFile(filename, callback) {
-  fs.readFile(filename, function (err, data) {
-    if(err) {
-      callback(err);
-      return;
-    }
-    try {
-      callback(null, JSON.parse(data));
-    } catch(exception) {
-      callback(exception);
-    }
-  });
+fs.readFile(filename, function (err, data) {
+  if(err) {
+    callback(err);
+    return;
+  }
+  try {
+    callback(null, JSON.parse(data));
+  } catch(exception) {
+    callback(exception);
+  }
+});
 }
 
 app.get('/', function (req, res) {
-    readJSONFile('results.json', function (err, json) {
-      if(err) { throw err; }
-        res.send(json)
-    });
+  readJSONFile('results.json', function (err, json) {
+    if(err) { throw err; }
+      res.send(json)
+  });
 })
 
 app.listen(3000)
