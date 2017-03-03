8e11166a8f0237d96cb41987413eb66b1c512dc2
Carkib <bofoc52@gmail.com>
2017-03-03 14:40:51 -0500

    adding occupied parcometer test, still need some work though

--- src/test/java/ca/ingeno/hackothon/tester/tests/occupiedParkingTests.java
+++ src/test/java/ca/ingeno/hackothon/tester/tests/occupiedParkingTests.java
@@ -19,28 +19,80 @@ public class occupiedParkingTests
 
     private static final String NEAREST_LAT_BEFORE_11 = "46.8130846391239";
 
-    private static final String BORIS_LOCATION_CLOSEST = "/nearest/?location=-71.221213,46.812228";
+    private static final String NEAREST_LONG_BEFORE_13 = "-71.2208147255085";
+
+    private static final String NEAREST_LAT_BEFORE_13 = "46.8131286965589";
+
+    private static final String BORIS_LOCATION_CLOSEST = "/nearest?location=-71.221213,46.812228";
+
+    private static final String PARKING_ESTIMAUVILLE_LONG = "-71.2112434853845";
+
+    private static final String PARKING_ESTIMAUVILLE_LAT = "46.8427720148002";
+
+    private static final String A_PARKING_NEAR_ESTIMAUVILLE = "/nearest?location=-71.2112562302213,46.8427830011235";
+
+    private static final String A_PARKING_SAINT_PAUL_LONG = "-71.214475345103";
+
+    private static final String A_PARKING_SAINT_PAUL_LAT = "46.8168303308662";
+
+    private static final String SECOND_PARKING_SAINT_PAUL_LONG = "-71.2144677710821";
+
+    private static final String SECOND_PARKING_SAINT_PAUL_LAT = "46.8168194925062";
+
+    private static final String A_PARKING_NEAR_SAINT_PAUL = "/nearest?location=-71.214475345103,46.8168303308662";
 
     private occupiedParkingTests()
     {
     }
 
-    public static TestResult nearestAtAnyTimeReturnTheAppropriateParking(String endpoint)
+    public static TestResult nearestAtAnyTimeReturnTheAppropriateParkingEasy(String endpoint)
     {
         boolean result = false;
         if (getHoursOfTheDay() < 10)
         {
-            result = returnTheClosestPoint(endpoint, NEAREST_LONG_BEFORE_10, NEAREST_LAT_BEFORE_10);
+            result = returnTheClosestPointBoris(endpoint, NEAREST_LONG_BEFORE_10, NEAREST_LAT_BEFORE_10);
         }
 
         if (getHoursOfTheDay() >= 10 && getHoursOfTheDay() < 11)
         {
-            result = returnTheClosestPoint(endpoint, NEAREST_LONG_BEFORE_11,  NEAREST_LAT_BEFORE_11);
+            result = returnTheClosestPointBoris(endpoint, NEAREST_LONG_BEFORE_11,  NEAREST_LAT_BEFORE_11);
         }
+
+        if (getHoursOfTheDay() >= 11)
+        {
+            result = returnTheClosestPointBoris(endpoint, NEAREST_LONG_BEFORE_13,  NEAREST_LAT_BEFORE_13);
+        }
+
+        if (getHoursOfTheDay() >= 16)
+        {
+            result = result && returnTheFinalClosestPoint(endpoint);
+        }
+
         return new TestResult("The nearest route, at any time, return the appropriate parking", result);
     }
 
-    private static boolean returnTheClosestPoint(String endpoint, String longi, String lat)
+    public static TestResult nearestAtAnytimeReturnTheAppropriateParkingMedium(String endpoint)
+    {
+        boolean result = false;
+        if (getHoursOfTheDay() < 10)
+        {
+            result = returnTheClosestPointSaintPaul(endpoint, A_PARKING_SAINT_PAUL_LONG, A_PARKING_SAINT_PAUL_LAT);
+        }
+
+        if (getHoursOfTheDay() >= 10 && getHoursOfTheDay() < 13)
+        {
+            result = returnTheClosestPointSaintPaul(endpoint, SECOND_PARKING_SAINT_PAUL_LONG,  SECOND_PARKING_SAINT_PAUL_LAT);
+        }
+
+        if (getHoursOfTheDay() >= 13)
+        {
+            result = returnTheClosestPointSaintPaul(endpoint, A_PARKING_SAINT_PAUL_LONG,  A_PARKING_SAINT_PAUL_LAT);
+        }
+
+        return new TestResult("The nearest route, at any time, return the appropriate parking", result);
+    }
+
+    private static boolean returnTheClosestPointBoris(String endpoint, String longi, String lat)
     {
         try
         {
@@ -53,12 +105,26 @@ public class occupiedParkingTests
         }
     }
 
-    private static boolean returnTheClosestPointBefore11(String endpoint)
+
+    private static boolean returnTheClosestPointSaintPaul(String endpoint, String longi, String lat)
     {
         try
         {
-            List<JsonNode> list_of_points = getFeaturesAtURL("http://" + endpoint + BORIS_LOCATION_CLOSEST);
-            return listContainTheClosestPoint(list_of_points);
+            List<JsonNode> list_of_points = getFeaturesAtURL("http://" + endpoint + A_PARKING_NEAR_SAINT_PAUL);
+            return listContainTheClosestPoint(list_of_points,longi, lat);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    private static boolean returnTheFinalClosestPoint(String endpoint)
+    {
+        try
+        {
+            List<JsonNode> list_of_points = getFeaturesAtURL("http://" + endpoint + A_PARKING_NEAR_ESTIMAUVILLE);
+            return listContainTheClosestPoint(list_of_points, PARKING_ESTIMAUVILLE_LONG, PARKING_ESTIMAUVILLE_LAT);
         }
         catch (Exception e)
         {
